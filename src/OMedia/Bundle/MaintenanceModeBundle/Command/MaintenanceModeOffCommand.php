d02c3bb01e3c4513627e983c402bfc181bb1baed
Alexander Sergeychik <alexander.sergeychik@gmail.com>
2013-11-19 19:35:03 +0300

    Small fix of command descriptions

--- src/OMedia/Bundle/MaintenanceModeBundle/Command/MaintenanceModeOffCommand.php
+++ src/OMedia/Bundle/MaintenanceModeBundle/Command/MaintenanceModeOffCommand.php
@@ -10,55 +10,55 @@ use Symfony\Component\Console\Output\OutputInterface;
  * @author Alexander Sergeychik
  */
 class MaintenanceModeOffCommand extends AbstractCommand {
-	
+
 	/**
 	 * (non-PHPdoc)
-	 * 
+	 *
 	 * @see \OMedia\Bundle\MaintenanceModeBundle\Command\AbstractCommand::configure()
 	 */
 	protected function configure() {
 		parent::configure();
 		$this->setName('maintenance:off');
-		$this->setDescription('Sets maintenance mode off');
+		$this->setDescription('Turns maintenance mode Off');
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
-	 * 
+	 *
 	 * @see \Symfony\Component\Console\Command\Command::execute()
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		
+
 		// back original .htaccess
 		$oldHtaccess = $this->getTargetFilename($input, '.htaccess.maintenance');
 		$newHtaccess = $this->getTargetFilename($input, '.htaccess');
-		
+
 		if (file_exists($oldHtaccess)) {
 			$oldContent = file_get_contents($oldHtaccess);
 			if ($oldContent === false) {
 				throw new \RuntimeException(sprintf('Unable to read %s', $oldHtaccess));
 			}
-			
+
 			$result = file_put_contents($newHtaccess, $oldContent);
 			if ($result === false) {
 				throw new \RuntimeException(sprintf('Unable to write %s', $newHtaccess));
 			}
-			
+
 			$result = unlink($oldHtaccess);
 			if ($result === false) {
 				throw new \RuntimeException(sprintf('Unable to delete %s', $oldHtaccess));
 			}
-		
+
 		} else {
 			$output->writeln(sprintf('<error>Warning! %s not exists, maybe maintenance mode already off?</error>', $newHtaccess));
 		}
-		
+
 		$targetFile = $this->getTargetFilename($input);
 		$result = unlink($targetFile);
 		if ($result === false) {
 			throw new \RuntimeException(sprintf('Unable to delete %s', $targetFile));
 		}
-		
+
 		$output->writeln('Maintenance mode is now <comment>off</comment>');
 	}
 
--- src/OMedia/Bundle/MaintenanceModeBundle/Command/MaintenanceModeOnCommand.php
+++ src/OMedia/Bundle/MaintenanceModeBundle/Command/MaintenanceModeOnCommand.php
@@ -11,27 +11,27 @@ use Symfony\Component\Console\Input\InputOption;
  * @author Alexander Sergeychik
  */
 class MaintenanceModeOnCommand extends AbstractCommand {
-	
+
 	/**
 	 * (non-PHPdoc)
-	 * 
+	 *
 	 * @see \OMedia\Bundle\MaintenanceModeBundle\Command\AbstractCommand::configure()
 	 */
 	protected function configure() {
 		parent::configure();
 		$this->setName('maintenance:on');
-		$this->setDescription('Sets maintenance mode on');
-		
+		$this->setDescription('Turns maintenance mode On');
+
 		$this->addOption('reason', null, InputOption::VALUE_OPTIONAL, 'Reason entering maintenance mode');
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
-	 * 
+	 *
 	 * @see \Symfony\Component\Console\Command\Command::execute()
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		
+
 		// Prepare template
 		/* @var $engine \Symfony\Component\Templating\EngineInterface */
 		$engine = $this->getContainer()->get('templating');
@@ -39,21 +39,21 @@ class MaintenanceModeOnCommand extends AbstractCommand {
 		$pageContent = $engine->render('MaintenanceModeBundle::maintenance.html.twig', array(
 			'reason' => $reason
 		));
-		
+
 		// Prepare htaccess
 		$htaccessContent = $engine->render('MaintenanceModeBundle::maintenance.htaccess.twig');
-		
+
 		// put template into web root
 		$targetFilename = $this->getTargetFilename($input);
 		$result = file_put_contents($targetFilename, $pageContent);
 		if ($result === false) {
 			throw new \RuntimeException(sprintf('Unable to write %s', $targetFilename));
 		}
-		
+
 		// replace .htaccess
 		$oldHtaccess = $this->getTargetFilename($input, '.htaccess');
 		$newHtaccess = $this->getTargetFilename($input, '.htaccess.maintenance');
-		
+
 		if (!file_exists($newHtaccess)) {
 			$result = copy($oldHtaccess, $newHtaccess);
 			if ($result === false) {
@@ -62,12 +62,12 @@ class MaintenanceModeOnCommand extends AbstractCommand {
 		} else {
 			$output->writeln(sprintf('<error>Warning! %s already exists, maybe maintenance mode already on?</error>', $newHtaccess));
 		}
-		
+
 		$result = file_put_contents($oldHtaccess, $htaccessContent);
 		if ($result === false) {
 			throw new \RuntimeException(sprintf('Unable to renew %s with maintenance content', $oldHtaccess));
 		}
-		
+
 		$output->writeln('Maintenance mode is now <comment>on</comment>');
 	}
 
